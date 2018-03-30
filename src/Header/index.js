f40853a4f0e2702b70484cfad4f67f07a0e06ae8
Vivek Pawar <Vivek.Pawar@walmart.com>
2018-03-31 01:12:08 +0530

    Added header component

---
+++ src/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import headerStyles from './Header.style'
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = headerStyles
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.title}</Text>
+    </View>
+  );
+}
+
+export default Header
