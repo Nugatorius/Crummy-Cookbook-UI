47aa9089ea3712684a89bf819e42a10f4f67533c
Ar11rA <aritra.ghosh2295@yahoo.com>
2018-03-31 16:04:08 +0530

    Counter Redux sample

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@
 
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
-import Header from './Header';
-import Counter from './Counter';
+import Header from './Components/Header';
+import CounterRedux from './Components/CounterRedux';
+const store = createStore(reducers, {});
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Header title="Crummy Cook recipes" />
-        <Counter />
-      </View>
+      <Provider store={store}>
+        <View>
+          <Header title="Crummy Cook recipes" />
+          <CounterRedux />
+        </View>
+      </Provider>
     );
   }
 }
